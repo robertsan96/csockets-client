4bc06643ff4c6b10a63b04644a0a2772fb0d473a
RobertSan <robertsandru96@gmail.com>
2017-03-09 08:16:51 +0200

    First commit

---
+++ CSocketsClient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSocketsClient
+//
+//  Created by Robert Sandru on 3/9/17.
+//  Copyright © 2017 Robert Sandru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
